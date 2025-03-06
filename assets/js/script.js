469f7f344772c6e06cd62b278379be1605d0b2a9
Caleb Ese Oru <orucaleb@gmail.com>
2025-03-06 12:14:20 +0100

    update all files in the input dirrectory

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
+ function toggleVideo() {
+    const trailer = document.querySelector('.trailer');
+    const video = document.querySelector('video');
+    video.pause();
+    trailer.classList.toggle('active');
+  }
+
+  function changeBg(bg, title) {
+    const banner = document.querySelector('.banner');
+    const contents = document.querySelectorAll('.content');
+    banner.style.background = `url(../assets/img/${bg})`;
+    banner.style.backgroundSize = 'cover';
+    banner.style.backgroundPosition = 'center';
+
+    contents.forEach(content => {
+      content.classList.remove('active');
+      if (content.classList.contains(title)) {
+        content.classList.add('active');
+      }
+    });
+  }
+
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems, options);
